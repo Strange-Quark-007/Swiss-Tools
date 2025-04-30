c765d830d1c464f3fc163288dbf3c68a292498e1
Strange-Quark-007 <masteryasin@ymail.com>
2025-04-30 23:23:40 +0530

    feat: implement middleware and enhance layout with dynamic title
    - Add middleware to enrich request headers with URL information.
    - Update RootLayout to utilize dynamic title generation based on the current pathname.
    - Introduce StringUtils class for string manipulation and formatting.
    - Update localization messages

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const url = new URL(request.url);
+  const origin = url.origin;
+  const pathname = url.pathname;
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-url', request.url);
+  requestHeaders.set('x-origin', origin);
+  requestHeaders.set('x-pathname', pathname);
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
+
+export const config = {
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
+};
