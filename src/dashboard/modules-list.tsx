dc5c3680e75514b2b4356596b5a5c8ca27a85b11
Yasin  Bhimani <masteryasin@ymail.com>
2025-09-25 10:00:59 +0530

    feat: refactor dashboard to use ModulesList component and update translations for improved clarity (#43)

---
+++ src/dashboard/modules-list.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { MouseEvent } from 'react';
+import { Star } from 'lucide-react';
+
+import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { Heading } from '@/components/typography/heading';
+import { Text } from '@/components/typography/text';
+import * as Types from '@/types/app-module';
+import { useAppStore } from '@/store/store';
+import { useT } from '@/i18n/utils';
+import { cn } from '@/lib/utils';
+import { appModules } from '@/constants/appModules';
+import { useModuleNavigation } from '@/hooks/use-module-navigation';
+
+const ModuleCard = ({ id, icon: Icon, name, description }: Types.AppModuleItem) => {
+  const t = useT();
+  const navigate = useModuleNavigation();
+  const { favorites, addFavorite, removeFavorite } = useAppStore();
+
+  const isFavorite = favorites.includes(id);
+
+  const handleFavorite = (e: MouseEvent<SVGSVGElement>) => {
+    e.stopPropagation();
+    if (isFavorite) {
+      removeFavorite(id);
+    } else {
+      addFavorite(id);
+    }
+  };
+
+  return (
+    <Card
+      key={id}
+      onClick={() => navigate(id)}
+      className="h-22 flex flex-row items-center p-4 rounded-lg border hover:shadow-lg hover:border-primary hover:cursor-pointer gap-4"
+    >
+      <div className="p-2 rounded-md bg-secondary">
+        <Icon />
+      </div>
+      <div className="flex flex-1 flex-col gap-2 min-w-0">
+        <CardTitle className="truncate">{name}</CardTitle>
+        <CardContent className="px-0">
+          <Text variant="small" muted className="line-clamp-2 text-wrap truncate">
+            {description}
+          </Text>
+        </CardContent>
+      </div>
+      <Star
+        aria-label={isFavorite ? t('label.addFavorite') : t('label.removeFavorite')}
+        className={cn(
+          'w-5',
+          isFavorite ? 'fill-accent-foreground hover:fill-primary-foreground' : 'hover:fill-primary'
+        )}
+        onClick={handleFavorite}
+      />
+    </Card>
+  );
+};
+
+export const ModulesList = () => {
+  const t = useT();
+  const modules = appModules(t);
+  return (
+    <>
+      {modules.map((group) => (
+        <section key={group.label} className="flex flex-col gap-4">
+          <Heading level={2} className="tracking-wide">
+            {group.label}
+          </Heading>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {group.items.map((item) => (
+              <ModuleCard key={item.id} {...item} />
+            ))}
+          </div>
+        </section>
+      ))}
+    </>
+  );
+};
