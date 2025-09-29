82aac2cc9248ee984be110f916f38b1a43677d1d
Strange-Quark-007 <masteryasin@ymail.com>
2025-09-29 22:30:59 +0530

    style: simplify Dashboard layout by removing unnecessary wrapper div around ModulesList

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -9,16 +9,14 @@ export const Dashboard = async () => {
   const t = await getT();
 
   return (
-    <FlexContainer direction="col" className="p-16">
+    <FlexContainer direction="col" className="p-16 gap-16">
       <section className="flex flex-col gap-6 items-center text-center bg-background">
         <Heading className="text-4xl md:text-5xl font-bold tracking-tight max-w-3xl text-balance">
           {t('dashboard.title')}
         </Heading>
         <Paragraph className="text-lg font-medium text-balance max-w-2xl">{t('dashboard.paragraph')}</Paragraph>
       </section>
-      <div className="flex flex-col gap-16">
-        <ModulesList />
-      </div>
+      <ModulesList />
     </FlexContainer>
   );
 };
